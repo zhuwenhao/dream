182805424724a0bac33c6dc559d502d4d9175c78
zhuwenhao <m@zhuwenhao.me>
2023-12-17 22:18:32 +0800

    ...

--- routes/bing/index.dart
+++ routes/bing/index.dart
@@ -7,13 +7,18 @@ import 'package:dream/models/bing.dart';
 Future<Response> onRequest(RequestContext context) async {
   try {
     final res = await dio.Dio().get<String>(
-      'https://bing.com/HPImageArchive.aspx?format=js&n=1&mkt=zh-CN',
+      'https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN',
       options: dio.Options(responseType: dio.ResponseType.plain),
     );
     final json = jsonDecode(res.data.toString()) as Map<String, dynamic>;
     final bing = Bing.fromJson(json);
 
-    return Response(body: 'https://bing.com${bing.images.first.url}');
+    return Response(
+      statusCode: 302,
+      headers: {
+        'Location': 'https://bing.com${bing.images.first.url}',
+      },
+    );
   } catch (e) {
     return Response(statusCode: 500);
   }
